8fdf696dfe8f609b92a7929987e18b7b61fdc3d3
Ankur Papneja <ankurpapneja4@gmail.com>
2024-04-24 23:58:17 +0530

    Api Gateway setup

---
+++ api-gateway/src/main/java/com/github/ankurpapneja4/bookkeeper/gateway/config/RouteConfig.java
@@ -0,0 +1,20 @@
+package com.github.ankurpapneja4.bookkeeper.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator routeFunction(RouteLocatorBuilder routeBuilder){
+        return routeBuilder.routes()
+                    .route( "accounts-service",
+                            r -> r.path("/bookkeeper/accounts-service/**")
+                                    .filters( f -> f.rewritePath("/bookkeeper/(?<segment>.*)", "/${segment}") )
+                                      .uri("http://localhost:8080"))
+                .build();
+    }
+}
