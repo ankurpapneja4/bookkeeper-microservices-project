e3dd123b937551b1e266dbd4f4f11861012b5d72
Ankur Papneja <ankurpapneja4@gmail.com>
2024-04-28 13:25:34 +0530

    RestTemplate Added

--- accounts-service/src/main/java/com/github/ankurpapneja4/bookkeeper/inventoryservice/services/accountservice/AccountService.java
+++ accounts-service/src/main/java/com/github/ankurpapneja4/bookkeeper/inventoryservice/services/accountservice/AccountService.java
@@ -1,14 +1,30 @@
 package com.github.ankurpapneja4.bookkeeper.inventoryservice.services.accountservice;
 
 import com.github.ankurpapneja4.bookkeeper.model.AccountDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 @Service("rest")
 public class AccountService {
 
+    private final RestTemplate restTemplate;
+
+    public AccountService(
+            @Value("${accountService.client.serviceUri}") String rootUri,
+            RestTemplateBuilder restTemplateBuilder ){
+
+        this.restTemplate = restTemplateBuilder.rootUri( rootUri ).build();
+    }
+
     public AccountDto createAccount( AccountDto accountDto ){
-        // Todo - Use RestTemplate To Call Account Service
-        return null;
+
+         URI location = restTemplate.postForLocation( "/api/v1/accounts", accountDto);
+
+         return restTemplate.getForObject( location, AccountDto.class);
     }
 
 
