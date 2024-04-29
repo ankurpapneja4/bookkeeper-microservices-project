ce64f8a0a9737106cb1272951e1c8be5877ff113
Ankur Papneja <ankurpapneja4@gmail.com>
2024-04-29 23:36:02 +0530

    maven bom updated

---
+++ eureka-service/src/test/java/com/github/ankurpapneja4/bookkeeper/eureka/EurekaServerApplicationTest.java
@@ -0,0 +1,14 @@
+package com.github.ankurpapneja4.bookkeeper.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTest {
+
+    @Test
+    void contextLoads(){
+        System.out.println( "Context Loads" );
+    }
+
+}
