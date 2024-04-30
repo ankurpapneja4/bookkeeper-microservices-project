d0a44deaf9a04d67468f5c7f9765fff21218ff51
Ankur Papneja <ankurpapneja4@gmail.com>
2024-04-30 20:11:46 +0530

    api-gateway | new routes added

--- api-gateway/src/main/java/com/github/ankurpapneja4/bookkeeper/gateway/config/RouteConfig.java
+++ api-gateway/src/main/java/com/github/ankurpapneja4/bookkeeper/gateway/config/RouteConfig.java
@@ -1,6 +1,5 @@
 package com.github.ankurpapneja4.bookkeeper.gateway.config;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +14,23 @@ public class RouteConfig {
                     .route( "accounts-service",
                             r -> r.path("/bookkeeper/accounts-service/**")
                                     .filters( f -> f.rewritePath("/bookkeeper/accounts-service/(?<segment>.*)", "/${segment}") )
-                                      .uri("lb://accounts-service"))
+                                      .uri("lb://accounts-service")
+                    )
+                    .route( "inventory-service",
+                            r -> r.path("/bookkeeper/inventory-service/**")
+                                    .filters( f -> f.rewritePath("/bookkeeper/inventory-service/(?<segment>.*)","/${segment}"))
+                                        .uri("lb://inventory-service")
+                    )
+                    .route( "purchase-service", r ->
+                            r.path("/bookkeeper/purchase-service/**")
+                                    .filters( f -> f.rewritePath("/bookkeeper/purchase-service/(?<segment>.*)","/{segment}"))
+                                        .uri("lb://purchase-service")
+                    )
+                    .route( "tax-service",
+                            r -> r.path("/bookkeeper/tax-service/**")
+                                    .filters(f -> f.rewritePath("/bookkeeper/tax-service/**","/${segment}"))
+                                        .uri("lb://tax-service")
+                    )
                 .build();
     }
 }
