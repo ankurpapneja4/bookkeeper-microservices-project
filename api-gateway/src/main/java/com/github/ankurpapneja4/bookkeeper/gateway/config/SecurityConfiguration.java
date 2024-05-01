f74c8ad051bf322235a8d1003d57293aa34994e2
Ankur Papneja <ankurpapneja4@gmail.com>
2024-05-02 00:58:20 +0530

    api-gateway | Disable csrf token exchange

--- api-gateway/src/main/java/com/github/ankurpapneja4/bookkeeper/gateway/config/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/github/ankurpapneja4/bookkeeper/gateway/config/SecurityConfiguration.java
@@ -1,9 +1,24 @@
 package com.github.ankurpapneja4.bookkeeper.gateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebFluxSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration{
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http){
+
+        http.authorizeExchange( exchanges -> exchanges.anyExchange().authenticated() )
+                .httpBasic( withDefaults() )
+                .csrf( csrfSpec -> csrfSpec.disable() );
+
+        return http.build();
+    }
 }
