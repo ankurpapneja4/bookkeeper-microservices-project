9fd3d215bf7903de9112d9c3d9908704505f95af
Ankur Papneja <ankurpapneja4@gmail.com>
2024-05-02 02:22:19 +0530

    continuous delivery support | docker push

--- build.cmd
+++ build.cmd
@@ -1,8 +1,8 @@
-call mvn -f ./eureka-service/pom.xml package -Pdocker
-call mvn -f ./api-gateway/pom.xml package -Pdocker
-call mvn -f ./config-server/pom.xml package -Pdocker
-call mvn -f ./accounts-service/pom.xml package -Pdocker
-call mvn -f ./inventory-service/pom.xml package -Pdocker
-call mvn -f ./purchase-service/pom.xml package -Pdocker
-call mvn -f ./tax-service/pom.xml package -Pdocker
+call mvn -f ./eureka-service/pom.xml install -Pdocker
+call mvn -f ./api-gateway/pom.xml install -Pdocker
+call mvn -f ./config-server/pom.xml install -Pdocker
+call mvn -f ./accounts-service/pom.xml install -Pdocker
+call mvn -f ./inventory-service/pom.xml install -Pdocker
+call mvn -f ./purchase-service/pom.xml install -Pdocker
+call mvn -f ./tax-service/pom.xml install -Pdocker
 
