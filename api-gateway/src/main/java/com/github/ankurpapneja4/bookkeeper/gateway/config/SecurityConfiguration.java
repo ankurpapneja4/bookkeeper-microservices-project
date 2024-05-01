e57ebf48a556f8ad4b398b336b90450565c349e7
Ankur Papneja <ankurpapneja4@gmail.com>
2024-05-01 20:20:26 +0530

    api-gateway | enable basic security

---
+++ api-gateway/src/main/java/com/github/ankurpapneja4/bookkeeper/gateway/config/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.ankurpapneja4.bookkeeper.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+}
